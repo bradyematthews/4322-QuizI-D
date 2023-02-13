3821d99aa1f2ca12cd13f11136bce0df65af1b53
Brady Matthews <brady_matthews1@baylor.edu>
2023-02-13 15:12:18 -0600

    Quiz Update 1

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -11,17 +11,56 @@ Iternate through the dictionary to print out their name and thier new salary (as
 import csv
 
 #open the file
+EMPFILE = 'employee_data.csv'
 
+infile = open(EMPFILE, 'r', newline = '')
 
+reader = csv.reader(infile, delimiter =',')
 
 
 #create an empty dictionary
-
+emp_dict = {}
 
 #use a loop to iterate through the csv file
+for row in reader: 
+    i = 0
+    first_name = row[1]
+    last_name = row[2]
+    dept = row[3]
+    old_salary = row[5]
+    i += 1
+
+    print("Manager Name:", (first_name), (last_name), "Dept:", (dept), 'Salary:', (old_salary))
+
+print(reader)
+
 
 
     #check if the employee fits the search criteria
+for i in reader:
+    if i[3] == 'Marketing':
+        emp_list = []
+
+        i = 0
+        first_name = row[1]
+        last_name = row[2]
+        dept = row[3]
+        old_salary = row[5]
+        i += 1
+        reader = []
+
+        emp_list.append(first_name)
+        emp_list.append(last_name)
+        emp_list.append(old_salary)
+        print("Manager Name:", (first_name), (last_name), 'Salary:', (old_salary))
+        emp_dict[i] = emp_list
+        new_salary = old_salary * 1.1
+
+        print("Manager Name:", (first_name), (last_name), 'Salary:', (new_salary))
+        emp_dict[i] = emp_list
+
+
+print(emp_dict)
 
 
     
